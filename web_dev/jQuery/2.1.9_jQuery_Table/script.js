4412656837222e93ea630e54af3430126e96c5b4
Creed Studenski <creedstudenski@Creeds-MacBook-Air.local>
2023-10-10 10:54:17 -0500

    jQuery Stretch goal solution Smart Table 2.1.10

--- web_dev/jQuery/2.1.9_jQuery_Table/script.js
+++ web_dev/jQuery/2.1.9_jQuery_Table/script.js
@@ -9,10 +9,34 @@ $(document).ready(function(){
     //tomato
 //research :nth-child
 
+
+//create function checkCost
+    //if cost graeter than 10$
+    //change background color to tomato
+//create a for loop
+    //:nth-child(num)
+//use parseFloat
+    //see if value <10
+    //convert the text value of column to a float
+//use .length to figure out how many elements are in an html file
+    //return number pf element
+    //iterate throught trs
+
 $("th").css("font-size","20px");
 $("th").css("font-style","italic");
 
-$("table td:nth-child(even)").css("background-color", "tomato");
 $("table td:nth-child(odd)").css("background-color", "lightblue");
+
+checkCost();
+
+function checkCost(){
+    for(var i=1; i<=$("tr").lenth; i++)
+    var item = $("tr:nth-child (" + i +") td:nth-child(2)");
+    var cost = parseFloat(item.text());
+    if(cost>10){
+        item.css("background-color","tomato");
+
+    }
+}
 
 });
